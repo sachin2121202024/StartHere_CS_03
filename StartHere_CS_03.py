298f25869b272bcdf52b74cfd918c88b3c00c158
sachin2121202024 <sachindevgantayat@gmail.com>
2024-10-21 19:44:00 +0530

    Add files via upload

---
+++ StartHere_CS_03.py
@@ -0,0 +1,27 @@
+import re
+def password_complexity_checker(password):
+
+    length_score = len(password)>=8
+    uppercase_score = bool(re.search(r'[A-Z]',password))
+    lowercase_score = bool(re.search(r'[a-z]',password))
+    number_score = bool(re.search(r'[0-9]',password))
+    special_char_score = bool(re.search(r'[!@#$%^&*(),.:"{}|<>?~]',password))
+
+    total_score = length_score + uppercase_score + lowercase_score + number_score + special_char_score
+
+    if total_score == 5:
+       return "Strong password"
+    elif total_score >= 3:
+       return "Good password"
+    else:
+       return "Weak password"
+
+password = input("Enter your password: ")
+conpassword = input("Confirm your password: ")
+
+if password == conpassword:
+   strength = password_complexity_checker(conpassword)
+   print(f"Password strength :  {strength}")
+else:
+   print("**Password didn't match**")
+   
